7e12d915e7c9c8f0fd3b3de12148f95a6334196e
Jason Tucker <jason@tucker.pro>
2014-01-11 14:36:45 -0800

    test
    test

--- deploy.sh
+++ deploy.sh
@@ -13,7 +13,7 @@ GITPATH="$CURRENTDIR/" # this file should be in the base of your git repository
 # svn config
 SVNPATH="/tmp/$PLUGINSLUG" # path to a temp SVN repo. No trailing slash required and don't add trunk.
 SVNURL="http://plugins.svn.wordpress.org/display-registered-image-dimensions/" # Remote SVN repo on wordpress.org, with no trailing slash
-SVNUSER="BFTrick" # your svn username
+SVNUSER="jasontucker" # your svn username
 
 
 # Let's begin...
